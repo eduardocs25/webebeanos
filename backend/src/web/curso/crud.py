ca268610e13d37d4e2db4827c653f4db6e1d705a
eduardocs25 <eduardom.hoyos@gmail.com>
2014-07-30 22:44:34 -0700

    push con canvas curso

--- backend/src/web/curso/crud.py
+++ backend/src/web/curso/crud.py
@@ -1,17 +1,24 @@
 from __future__ import  absolute_import, unicode_literals
 from zen import router
+from web.curso.rest import Curso
 __author__ = 'eduardo'
 #crud = create read update delete
 
-def index():
-    pass
+def index(_write_tmpl):
+    query = Curso.query().order(Curso.nome)
+    dct = {'lista_cursos':query.fetch()}
+    _write_tmpl('/templates/curso_listar.html', dct)
 
+#con esto se calcula la url
 def form(_write_tmpl):
     path = router.to_path(salvar)
+    #se crea un dicionario para conectar el html form con esto
     dct = {'salvar_curso':path}
     _write_tmpl('/templates/curso_form.html', dct)
 
-def salvar(nome,preco):
+def salvar(_handler, nome, preco):
     preco = float(preco)
     curso = Curso(nome = nome, preco = preco)
     curso.put()
+    path = router.to_path(index)
+    _handler.redirect(path)
